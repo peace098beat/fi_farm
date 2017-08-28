fa507f43768425ac5c00210b3aaa7b08cc085479
fifi <fi@fimail.com>
2017-08-28 13:13:07 +0000

    0.0.18

--- take_picture.sh
+++ take_picture.sh
@@ -4,4 +4,4 @@ photo_dir="/opt/fi_farm/data/photos"
 file_name=$(date +"%Y-%m-%d-%H%M%S").jpg
 
 echo "take_picture :  ${photo_dir}/${file_name}"
-raspistill -v -w 1280 -h 720 -o ${photo_dir}/${file_name}
+raspistill -w 1280 -h 720 -o ${photo_dir}/${file_name}
