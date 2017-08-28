112a841d31d2f3a43cbff7f585371e53e489bc2c
Tomoyuki Nohara a.k.a FiFi <peace098beat.t@gmail.com>
2017-08-28 05:07:39 +0000

    first commit

---
+++ take_picture.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+photo_dir="/opt/fi_farm/data/photos"
+file_name=$(date +"%Y-%m-%d-%H%M%S").jpg
+
+raspistill -w 1280 -h 720 -o ${photo_dir}/${file_name}
---
+++ uploader.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# upload metrics and photos.
+# this script check updates by AWS IoT shadow and replace source if the new source version is arrived
+base_dir="/opt/fi_farm"
+data_dir="${base_dir}/data"
+photo_dir="${data_dir}/photos/"
+
+
+
+
+
+# check environment variables
+if [ ! -e ${base_dir} ]; then
+  echo "no env file"
+  exit 1
+else
+  echo "yes env"
+fi
+
+
+# check environment variables
+if [ ! -e ${base_dir}/privatekey ]; then
+  echo "no env file"
+  exit 1
+fi
+source ${base_dir}/privatekey
+
+
+# TEST
+if [ -z "${S3_BUCKET+x}" ] ; then
+  echo "environment variable S3_BUCKET is requierd"
+  exit 1
+fi
+if [ -z "${AWS_REGION+x}" ] ; then
+  echo "environment variable AWS_REGION is requierd"
+  exit 1
+fi
+
+
+
+# Take Photo
+for img in `find $photo_dir -type f -name "*.jpg"`
+do
+  fname=`basename $img`
+  y=`echo $fname | cut -d "-" -f1`
+  m=`echo $fname | cut -d "-" -f2`
+  d=`echo $fname | cut -d "-" -f3`
+  echo "upload to s3://${S3_BUCKET}/${y}/${m}/${d}/$fname"
+  aws s3 cp $img s3://${S3_BUCKET}/${y}/${m}/${d}/$fname &> /dev/null
+  rm -f $img &> /dev/null
+done
